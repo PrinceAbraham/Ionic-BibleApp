2aaa3ebb9331730b1562175fcf8797e97d3deaac
PrinceAbraham <princeabraham@users.noreply.github.com>
2016-10-07 09:50:26 -0400

    commit rest

---
+++ www/js/controllers.js
@@ -0,0 +1,41 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, $http, $stateParams) {
+    if($stateParams.book){
+        $scope.bookName = $stateParams.book;
+    }
+    if($stateParams.books){
+        $scope.books = $stateParams.books;
+    }
+    $http.get('http://www.esvapi.org/v2/rest/passageQuery?key=071b2bef12d451fe&passage=Genesis+1&include-headings=false&include-surrounding-chapters=false').then(function(response){
+        console.log(response);
+        $scope.returned = response.data;
+        $scope.oldTestament = ["Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy", "Joshua", "Judges", "Ruth", "1 Samuel", "2 Samuel", "1 Kings", "2 Kings", "1 Chronicles", "2 Chronicles", "Ezra", "Nehemiah", "Esther", "Job", "Psalms", "Proverbs", "Ecclesiastes", "Song of Solomon", "Isaiah", "Jeremiah", "Lamentations", "Ezekiel", "Daniel", "Hosea", "Joel", "Amos", "Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk", "Zephaniah", "Haggai", "Zechariah", "Malachi"];
+        $scope.newTestament = ["Matthew", "Mark", "Luke", "John", "Acts", "Romans", "1 Corinthians", "2 Corinthians", "Galatians", "Ephesians", "Philippians", "Colossians", "1 Thessalonians", "2 Thessalonians", "1 Timothy", "2 Timothy", "Titus", "Philemon", "Hebrews", "James", "1 Peter", "2 Peter", "1 John", "2 John", "3 John", "Jude", "Revelation"];
+    });
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
